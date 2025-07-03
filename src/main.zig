9e781a65248f67d945b72200e4b3663cd776eebb
Tenyoru <red.blush729@passmail.com>
2025-07-03 20:13:25 +0200

    refactor: move slab fields into backend by storage type

--- src/main.zig
+++ src/main.zig
@@ -22,12 +22,15 @@ const SlabConfig = struct {
 pub fn createSlab(comptime T: type, comptime cfg: SlabConfig) type {
     return struct {
         const Self = @This();
-        allocator: std.mem.Allocator,
-        size: usize,
-        buckets_size: usize,
-        data: switch (cfg.storage) {
-            .dynamic => []?T,
-            .static => [cfg.size]?T,
+        backend: switch (cfg.storage) {
+            .dynamic => struct {
+                data: []?T,
+                allocator: std.mem.Allocator,
+                buckets_size: usize,
+            },
+            .static => struct {
+                data: [cfg.size]?T,
+            },
         },
 
         pub fn init(allocator: ?std.mem.Allocator) !Self {
@@ -40,68 +43,78 @@ pub fn createSlab(comptime T: type, comptime cfg: SlabConfig) type {
                     const default_size = 128;
                     const size = cfg.size orelse default_size;
 
-                    const data = try alloc.alloc(?T, size);
-                    @memset(data, null);
+                    const slots = try alloc.alloc(?T, size);
+                    @memset(slots, null);
                     return Self{
-                        .allocator = alloc,
-                        .data = data,
-                        .size = size,
-                        .buckets_size = 0,
+                        .backend = .{
+                            .allocator = alloc,
+                            .data = slots,
+                            .buckets_size = 0,
+                        },
                     };
                 },
                 .static => {
                     if (comptime cfg.size <= 0)
                         @compileError("Invalid slab size: cfg.size must be a positive integer greater than zero.");
                     return Self{
-                        .buckets_size = undefined,
-                        .allocator = undefined,
-                        .data = @splat(null),
-                        .size = cfg.size,
+                        // ..data = @splat(null),
+                        .backend = .{
+                            .data = @splat(null),
+                        },
                     };
                 },
             }
         }
 
-        pub fn len(self: *Self) usize {
-            return self.data.len;
+        pub inline fn len(self: *Self) usize {
+            return self.backend.data.len;
+        }
+
+        inline fn data(self: *Self) []?T {
+            return self.backend.data[0..];
+        }
+
+        //copy
+        inline fn at(self: *Self, index: usize) ?T {
+            return self.data()[index];
         }
 
         pub fn deinit(self: *Self) void {
             if (cfg.storage == .dynamic) {
-                self.allocator.free(self.data);
+                self.backend.allocator.free(self.data());
             }
         }
 
         inline fn checkOutOfRage(self: *Self, index: usize) !void {
-            if (cfg.safe and index >= self.size) return SlabError.OutOfRange;
+            if (cfg.safe and index >= self.len()) return SlabError.OutOfRange;
         }
 
         pub fn insertAt(self: *Self, index: usize, value: T) SlabError!void {
             try self.checkOutOfRage(index);
-            if (self.data[index] != null) return SlabError.SlotOccupied;
-            self.data[index] = value;
+            if (self.data()[index] != null) return SlabError.SlotOccupied;
+            self.data()[index] = value;
         }
 
         pub fn remove(self: *Self, index: usize) SlabError!void {
-            if (comptime cfg.safe and self.data[index] == null) return SlabError.RemoveEmptySlot;
-            self.data[index] = null;
+            if (comptime cfg.safe and self.data()[index] == null) return SlabError.RemoveEmptySlot;
+            self.data()[index] = null;
         }
 
         pub fn get_ptr(self: *Self, index: usize) SlabError!*const T {
             try self.checkOutOfRage(index);
 
-            const maybe = self.data[index] orelse return SlabError.EmptySlot;
+            const maybe = self.data()[index] orelse return SlabError.EmptySlot;
             return &maybe;
         }
 
         pub fn get(self: *Self, index: usize) SlabError!T {
             try self.checkOutOfRage(index);
 
-            return self.data[index] orelse SlabError.EmptySlot;
+            return self.data()[index] orelse SlabError.EmptySlot;
         }
 
-        inline fn findFreeSlot(self: *Self) !usize {
-            for (self.data, 0..) |bucket, i| {
+        pub fn findFreeSlot(self: *Self) !usize {
+            for (self.data(), 0..) |bucket, i| {
                 if (bucket == null)
                     return i;
             }
@@ -110,7 +123,8 @@ pub fn createSlab(comptime T: type, comptime cfg: SlabConfig) type {
 
         pub fn insert(self: *Self, value: T) !usize {
             const index: usize = try self.findFreeSlot();
-            self.data[index] = value;
+            if (self.data()[index] != null) return SlabError.SlotOccupied;
+            self.data()[index] = value;
             return index;
         }
     };
@@ -129,6 +143,6 @@ test "init dynamic" {
     var slab = try Slab.init(std.testing.allocator);
     slab.deinit();
     try slab.insertAt(2500, 40);
-    const value = try slab.get_ptr(2500);
-    try expectEqual(value.*, 40);
+    const value = try slab.get(2500);
+    try expectEqual(value, 40);
 }
