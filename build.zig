f7de0cbaec75ba3442b0c18412762b5d641c65ba
Tenyoru <red.blush729@passmail.com>
2025-11-04 00:42:34 +0100

    Run tests by default in build

--- build.zig
+++ build.zig
@@ -26,4 +26,6 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    b.default_step.dependOn(&run_lib_unit_tests.step);
 }
